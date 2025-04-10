a1900db37b1b9e80092aa6b05cd1ca980ab1faa2
ReneHosselet <renehosselet@gmail.com>
2025-04-10 23:30:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // This is the default value for development
+  publicDir: 'public', // This is also the default, can be omitted if using 'public'
 })
